3c20c5112d123d1f6ffe910629c161b1f18a982f
Fernando Freitas <freitax.li@gmail.com>
2024-05-14 22:52:28 -0300

    chore: Added default Section styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,42 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import SubHeader from "@/components/text/SubHeader/SubHeader";
 import Header from "@/components/text/Header/Header";
+import Section from "@/components/division/section/Section";
 
 export default function Home() {
   return (
-    <main>
-      <Header>
-        Header
-      </Header>
-      
-      <SubHeader>
-        SubHeader is a very nice type of tag!
-      </SubHeader>
-      <p>Olá mundooo</p>
+    <main id="main">
+      <SubHeader>SubHeader is a very nice type of tag!</SubHeader>
+      <Section id="section1">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
+          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
+          hic, eius odit. Libero natus doloribus delectus amet labore
+          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
+          blanditiis odit.
+        </p>
+      </Section>
+      <Section id="section2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
+          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
+          hic, eius odit. Libero natus doloribus delectus amet labore
+          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
+          blanditiis odit.
+        </p>
+      </Section>
+      <Section id="section3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
+          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
+          hic, eius odit. Libero natus doloribus delectus amet labore
+          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
+          blanditiis odit.
+        </p>
+      </Section>
     </main>
   );
 }
--- src/components/division/section/Section.tsx
+++ src/components/division/section/Section.tsx
@@ -1,10 +1,12 @@
+import styles from "./Section.module.css";
+
 interface Props {
   children: React.ReactNode | React.ReactNode[];
   id: string;
 }
 
 const Section = ({ children, id }: Props) => {
-  return <section id={id}>{children}</section>;
+  return <section className={styles.container} id={id}>{children}</section>;
 };
 
 export default Section;
