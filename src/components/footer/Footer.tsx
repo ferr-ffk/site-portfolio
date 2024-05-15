e24630ba68476bee463f31853d79c32e2a0a4af9
Fernando Freitas <freitax.li@gmail.com>
2024-05-15 12:17:40 -0300

    fix: Changed logo to name logo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,40 +1,48 @@
+import styles from "./Footer.module.css";
+
 import { FaGithub, FaItchIo, FaLinkedinIn } from "react-icons/fa6";
 import { MdOutlineMail } from "react-icons/md";
-import Logo from "../logo/Logo";
 
-import styles from "./Footer.module.css";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+const Contour_Generator = localFont({ src: "../../../public/font/CONTOUR-GENERATOR.ttf" });
 
 const links = [
   {
     href: "https://github.com/ferr-ffk",
     icon: <FaGithub />,
-    key: "github"
+    key: "github",
   },
   {
     href: "mailto:freitaslira18@gmail.com",
     icon: <MdOutlineMail />,
-    key: "email"
+    key: "email",
   },
   {
     href: "https://feh1000000.itch.io",
     icon: <FaItchIo />,
-    key: "itch"
+    key: "itch",
   },
   {
     href: "https://www.linkedin.com/in/fernandoflira/",
     icon: <FaLinkedinIn />,
-    key: "linkedin"
+    key: "linkedin",
   },
 ];
 
 const Footer = () => {
   return (
     <footer className={styles.container}>
-      <Logo />
+      <Link href="/" className={`${styles.logo} ${Contour_Generator.className}`}>
+        fern
+      </Link>
       <ul className={styles.links} id="links">
         {links.map((link) => (
           <li key={link.key}>
-            <a target="_blank" href={link.href}>{link.icon}</a>
+            <a target="_blank" href={link.href}>
+              {link.icon}
+            </a>
           </li>
         ))}
       </ul>
