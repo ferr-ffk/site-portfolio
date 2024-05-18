bf03c2d768c935cc8957a9931c80bb306e320da2
Fernando Freitas <freitax.li@gmail.com>
2024-05-18 11:19:05 -0300

    remove: Changed to placeholder logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,22 +1,15 @@
 import localFont from "next/font/local";
-import Image from "next/image";
-
-import NavbarLink from "../navbar/navbarLink/NavbarLink";
 
 import styles from "./Logo.module.css";
-
-interface Props {
-  header?: boolean;
-}
 
 const Contour_Generator = localFont({
   src: "../../../public/font/CONTOUR-GENERATOR.ttf",
 });
 
-const Logo = ({ header }: Props) => {
+const Logo = () => {
   return (
-    <div className={styles.container}>
-      <Image src="/logo/logo_feh_fritas.png" alt="logo" fill objectFit="contain"/>
+    <div className={`${styles.container} ${Contour_Generator.className}`}>
+      logo
     </div>
   );
 };
