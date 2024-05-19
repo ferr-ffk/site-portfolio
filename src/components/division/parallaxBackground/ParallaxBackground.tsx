24fd53f4266d6398749e9a7e842032c30b3f410d
Fernando Freitas <freitax.li@gmail.com>
2024-05-19 20:39:24 -0300

    chore: Improved parallax effect

--- src/components/division/parallaxBackground/ParallaxBackground.tsx
+++ src/components/division/parallaxBackground/ParallaxBackground.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import styles from "./ParallaxBackground.module.css";
 
 interface Props {
@@ -7,11 +9,13 @@ interface Props {
 }
 
 const ParallaxBackground = ({ frontUrl, backUrl, children }: Props) => {
+  addEventListener('scroll', handleScroll);
+
   return (
     <div className={styles.parallax}>
-      <img src={backUrl} alt="" className={styles.parallax__back} />
-      <img src={frontUrl} alt="" className={styles.parallax__front} />
-      <div className={styles.parallax__hero}>
+      <img id="parallax-back" src={backUrl} alt="" className={styles.parallax__back} />
+      <img id="parallax-front" src={frontUrl} alt="" className={styles.parallax__front} />
+      <div id="hero-text" className={styles.parallax__hero}>
         {children}
       </div>
     </div>
@@ -19,3 +23,24 @@ const ParallaxBackground = ({ frontUrl, backUrl, children }: Props) => {
 };
 
 export default ParallaxBackground;
+
+function handleScroll(this: HTMLElement, ev: Event) {
+  const scrollTop = document.documentElement.scrollTop;
+
+  const background = document.documentElement.querySelector<HTMLImageElement>('#parallax-back')!;
+  const foreground = document.documentElement.querySelector<HTMLImageElement>('#parallax-front')!;
+  const hero = document.documentElement.querySelector<HTMLDivElement>('#hero-text')!;
+
+  if (background) {
+    background.style.objectPosition = `0 ${scrollTop * -0.3}px`;
+  }
+
+  if (foreground) {
+    foreground.style.objectPosition = `0 ${scrollTop * 0.1}px`;
+  }
+
+  if (hero) {
+    hero.style.bottom = `${scrollTop * 0.5}px`;
+  }
+}
+
