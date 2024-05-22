9e84886debb675755983e50b57f0ea4448339bf9
Fernando Freitas <freitax.li@gmail.com>
2024-05-22 11:24:03 -0300

    chore: Addede Parallax support for mobile

--- src/components/division/parallaxBackground/ParallaxBackground.tsx
+++ src/components/division/parallaxBackground/ParallaxBackground.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import styles from "./ParallaxBackground.module.css";
 
@@ -9,12 +9,22 @@ interface Props {
 }
 
 const ParallaxBackground = ({ frontUrl, backUrl, children }: Props) => {
-  addEventListener('scroll', handleScroll);
+  addEventListener("scroll", handleScroll);
 
   return (
     <div className={styles.parallax}>
-      <img id="parallax-back" src={backUrl} alt="" className={styles.parallax__back} />
-      <img id="parallax-front" src={frontUrl} alt="" className={styles.parallax__front} />
+      <img
+        id="parallax-back"
+        src={backUrl}
+        alt=""
+        className={styles.parallax__back}
+      />
+      <img
+        id="parallax-front"
+        src={frontUrl}
+        alt=""
+        className={styles.parallax__front}
+      />
       <div id="hero-text" className={styles.parallax__hero}>
         {children}
       </div>
@@ -27,20 +37,21 @@ export default ParallaxBackground;
 function handleScroll(this: HTMLElement, ev: Event) {
   const scrollTop = document.documentElement.scrollTop;
 
-  const background = document.documentElement.querySelector<HTMLImageElement>('#parallax-back')!;
-  const foreground = document.documentElement.querySelector<HTMLImageElement>('#parallax-front')!;
-  const hero = document.documentElement.querySelector<HTMLDivElement>('#hero-text')!;
+  const background = document.documentElement.querySelector<HTMLImageElement>("#parallax-back")!;
+  const foreground = document.documentElement.querySelector<HTMLImageElement>("#parallax-front")!;
+  const hero = document.documentElement.querySelector<HTMLDivElement>("#hero-text")!;
 
   if (background) {
-    background.style.objectPosition = `0 ${scrollTop * -0.3}px`;
+    if (background.style.getPropertyValue('object-fit') !== "none") {
+      background.style.objectPosition = `0 ${scrollTop * -0.2}px`;
+    }
   }
 
   if (foreground) {
-    foreground.style.objectPosition = `0 ${scrollTop * 0.1}px`;
+    foreground.style.objectPosition = `0 ${scrollTop * 0.3}px`;
   }
 
   if (hero) {
     hero.style.bottom = `${scrollTop * 0.5}px`;
   }
 }
-
