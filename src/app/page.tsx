7d6af8cd13463d426707f9e5790874eefb935757
Fernando Freitas <freitax.li@gmail.com>
2024-05-23 19:18:39 -0300

    feat: created placholder projects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/content/hero/Hero";
 import About from "@/components/content/about/About";
+import Projects from "@/components/content/projects/Projects";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
     </>
   );
 }
---
+++ src/components/content/projects/Projects.tsx
@@ -0,0 +1,17 @@
+import Section from "@/components/division/section/Section";
+import SubHeader from "@/components/text/subHeader/SubHeader";
+
+const Projects = () => {
+  return (
+    <Section id="projetos" vertical>
+      <SubHeader>Projetos</SubHeader>
+      <div>
+        <div>projeto1</div>
+        <div>projeto2</div>
+        <div>projeto3</div>
+      </div>
+    </Section>
+  );
+};
+
+export default Projects;
