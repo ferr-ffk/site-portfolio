6860c374a255735fcfdeeb4484eda29e7f9be814
Fernando Freitas <freitax.li@gmail.com>
2024-05-23 19:18:21 -0300

    chore: added vertical layout support

--- src/components/division/section/Section.tsx
+++ src/components/division/section/Section.tsx
@@ -3,12 +3,15 @@ import styles from "./Section.module.css";
 interface Props {
   children: React.ReactNode | React.ReactNode[];
   id: string;
+  vertical?: boolean;
 }
 
-const Section = ({ children, id }: Props) => {
+const Section = ({ children, id, vertical = false }: Props) => {
   return (
     <section
-      className={`${styles.container} ${id == "hero" ? styles.hero : ""}`}
+      className={`${styles.container} ${id == "hero" ? styles.hero : ""} ${
+        vertical ? styles.vertical : ""
+      }`}
       id={id}
     >
       {children}
