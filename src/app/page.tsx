66e6fce22ef7602504e0818fdcc5368ce19169b6
Fernando Freitas <freitax.li@gmail.com>
2024-05-23 18:12:32 -0300

    feat: created basic About component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,12 @@
-import SubHeader from "@/components/text/subHeader/SubHeader";
 import Section from "@/components/division/section/Section";
 import Hero from "@/components/content/hero/Hero";
+import About from "@/components/content/about/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Section id="section">
-        <SubHeader>SubHeader is a very nice type of tag!</SubHeader>
-      </Section>
-      <Section id="sobre">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
-          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
-          hic, eius odit. Libero natus doloribus delectus amet labore
-          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
-          blanditiis odit.
-        </p>
-      </Section>
-      <Section id="section2">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
-          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
-          hic, eius odit. Libero natus doloribus delectus amet labore
-          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
-          blanditiis odit.
-        </p>
-      </Section>
-      <Section id="section3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          nisi illo, ratione doloremque provident quos ipsam ex libero et quod
-          minima voluptates unde dolore sequi veritatis magnam earum suscipit at
-          hic, eius odit. Libero natus doloribus delectus amet labore
-          accusantium, tempore itaque reprehenderit magni mollitia, ex cum error
-          blanditiis odit.
-        </p>
-      </Section>
-      <Section id="contato">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-          veritatis excepturi earum vero laborum quisquam eum quod quidem
-          doloremque, nesciunt qui nostrum ipsum? Enim voluptatum, quisquam nemo
-          cupiditate voluptates deleniti reiciendis a aspernatur nisi hic
-          pariatur omnis rem perferendis saepe adipisci cumque ab dignissimos
-          quo dolorem similique ea illum! Omnis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
-          facere voluptatem iste quam quaerat consequatur distinctio? Et facere
-          quos veritatis. Asperiores quia, qui enim itaque dolore nesciunt neque
-          distinctio laboriosam adipisci quam odio et voluptates, quisquam
-          molestias dolores fugiat harum inventore, corporis dolor libero.
-          Molestias alias pariatur assumenda laborum iste?
-        </p>
-      </Section>
+      <About />
     </>
   );
 }
---
+++ src/components/content/about/About.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Section from "@/components/division/section/Section";
+import SubHeader from "@/components/text/subHeader/SubHeader";
+
+const About = () => {
+  return (
+    <Section id="sobre">
+      <div>[IMAGE]</div>
+      <div>
+        <SubHeader>Sobre mim</SubHeader>
+        <p>
+          Eu me chamo Fernando. Eu tenho 17 anos e atualmente estou estudando o
+          ensino médio integrado ao técnico em informática no Instituto Federal
+          de São Paulo. No meu tempo livre passo ouvindo música e jogando. Eu
+          amo aprender, e estou sempre aprimorando minhas habilidades em coisas
+          que gosto como tecnologia e cozinhar, algo que estou sempre tentando
+          algo novo. Este aprendizado que gosto de compartilhar com os outros,
+          como já fui monitor de salas de estudo de lógica de programação onde
+          eu estudo, ajudando alunos com conhecimentos em lógica de programação.
+          Aguardo um dia no futuro em que posso prestar assistência a quem mais
+          precisa, principalmente na área de TI.
+        </p>
+      </div>
+    </Section>
+  );
+};
+
+export default About;
