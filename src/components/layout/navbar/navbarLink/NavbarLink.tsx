d877faa61d1a3b8b62128457d63ff973f65436cd
Fernando Freitas <freitax.li@gmail.com>
2024-05-26 13:40:27 -0300

    feat: added optional onClick event handler to NavbarLink

--- src/components/layout/navbar/navbarLink/NavbarLink.tsx
+++ src/components/layout/navbar/navbarLink/NavbarLink.tsx
@@ -1,4 +1,5 @@
 import { Oswald } from "next/font/google";
+import { MouseEventHandler } from "react";
 
 const oswald = Oswald({
   weight: "400",
@@ -8,11 +9,12 @@ const oswald = Oswald({
 interface Props {
   children: React.ReactNode;
   href: string;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
 }
 
-const NavbarLink = ({ children, href }: Props) => {
+const NavbarLink = ({ children, href, onClick }: Props) => {
   return (
-    <a href={href} className={oswald.className}>
+    <a href={href} className={oswald.className} onClick={onClick}>
       {children}
     </a>
   );
