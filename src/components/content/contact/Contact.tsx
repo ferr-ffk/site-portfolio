5d974627fcbaa861a445155fb6ed4634ba128545
Fernando Freitas <freitax.li@gmail.com>
2024-05-29 20:39:19 -0300

    feat: added basic TextArea component

--- src/components/content/contact/Contact.tsx
+++ src/components/content/contact/Contact.tsx
@@ -1,5 +1,6 @@
 import Section from "@/components/division/section/Section";
 import Input from "@/components/form/input/Input";
+import TextArea from "@/components/form/textArea/TextArea";
 import SubHeader from "@/components/text/subHeader/SubHeader";
 
 const Contact = () => {
@@ -10,6 +11,7 @@ const Contact = () => {
         <div>
           <Input name="to" id="to" placeholder="Para" value="freitaslira18@gmail.com" disabled/>
           <Input name="subject" id="subject" placeholder="Assunto" />
+          <TextArea name="content" id="content" />
         </div>
     </Section>
   );
