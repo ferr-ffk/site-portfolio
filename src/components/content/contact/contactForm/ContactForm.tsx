a16bd37a856aacebe91643d07b0af65be82f232d
Fernando Freitas <freitax.li@gmail.com>
2024-06-01 22:54:39 -0300

    feat: added email form

--- src/components/content/contact/contactForm/ContactForm.tsx
+++ src/components/content/contact/contactForm/ContactForm.tsx
@@ -1,31 +1,34 @@
-"use client"    
+"use client";
 
 import TextArea from "@/components/form/textArea/TextArea";
 import TextField from "@/components/form/textField/TextField";
-import LinkButton from "@/components/text/linkButton/LinkButton";
-import { ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent } from "react";
 
 const ContactForm = () => {
-  function handleEmailToChange(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error("Function not implemented.");
+  const router = useRouter();
+
+  let to: string;
+  let subject: string;
+  let content: string;
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+
+    router.push(`mailto:freitaslira18@gmail.com?subject=${subject}&body=${content}`);
   }
 
-  function handleEmailSubjectChange(
-    event: ChangeEvent<HTMLInputElement>
-  ): void {
-    throw new Error("Function not implemented.");
+  function handleSubjectChange(event: ChangeEvent<HTMLInputElement>): void {
+    subject = event.currentTarget.value;
   }
 
-  function handleEmailContentChange(
-    event: ChangeEvent<HTMLTextAreaElement>
-  ): void {
-    throw new Error("Function not implemented.");
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>): void {
+    content = event.currentTarget.value;
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <TextField
-        onChange={handleEmailToChange}
         name="to"
         id="to"
         placeholder="Para"
@@ -33,19 +36,13 @@ const ContactForm = () => {
         disabled
       />
       <TextField
-        onChange={handleEmailSubjectChange}
+        onChange={handleSubjectChange}
         name="subject"
         id="subject"
         placeholder="Assunto"
       />
-      <TextArea
-        onChange={handleEmailContentChange}
-        name="content"
-        id="content"
-      />
-      <LinkButton href="mailto:freitaslira18@gmail.com">
-        Enviar
-      </LinkButton>
+      <TextArea onChange={handleContentChange} name="content" id="content" />
+      <button type="submit">Enviar</button>
     </form>
   );
 };
