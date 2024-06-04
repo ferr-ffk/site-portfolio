b2a11e999b80a569ac9a6d78158ac9482632eaa9
Fernando Freitas <freitax.li@gmail.com>
2024-06-03 21:50:01 -0300

    feat: added delay for fade-in

--- src/components/layout/fadeIn/FadeIn.tsx
+++ src/components/layout/fadeIn/FadeIn.tsx
@@ -3,18 +3,31 @@
 import styles from "./FadeIn.module.css";
 
 import useOnScreen from "@/hook/useOnScreen";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   children?: React.ReactNode | React.ReactNode[];
+  delay?: number;
 }
 
-const FadeIn = ({ children }: Props) => {
+const FadeIn = ({ children, delay: stagger }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
   const isVisible = useOnScreen(ref);
 
+  const [className, setClassName] = useState(styles.hidden);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isVisible) {
+        setClassName(`${styles.hidden} ${styles.show}`);
+      } else {
+        setClassName(styles.hidden);
+      }
+    }, stagger);
+  }, [isVisible]);
+
   return (
-    <div ref={ref} className={`${styles.hidden} ${isVisible ? styles.show : ""}`}>
+    <div ref={ref} className={className}>
       {children}
     </div>
   );
