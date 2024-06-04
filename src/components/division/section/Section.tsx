1181eec7c5c15e90cb4ee6ad8370a55fc723e3cb
Fernando Freitas <freitax.li@gmail.com>
2024-06-03 22:11:53 -0300

    feat: added fade-in animation to Section children

--- src/components/division/section/Section.tsx
+++ src/components/division/section/Section.tsx
@@ -1,20 +1,35 @@
+import FadeIn from "@/components/layout/fadeIn/FadeIn";
 import styles from "./Section.module.css";
 
 interface Props {
   children: React.ReactNode | React.ReactNode[];
   id: string;
   vertical?: boolean;
+  disableAnimation?: boolean;
+  delayIncrease?: number;
 }
 
-const Section = ({ children, id, vertical = false }: Props) => {
+const Section = ({
+  children,
+  id,
+  vertical = false,
+  disableAnimation,
+  delayIncrease = 250,
+}: Props) => {
+  let delay = 0;
+
   return (
     <section
+      id={id}
       className={`${styles.container} ${id === "hero" ? styles.hero : ""} ${
         vertical ? styles.vertical : ""
       } ${id == "projetos" ? styles.projects : ""}`}
-      id={id}
     >
-      {children}
+      {!Array.isArray(children) || disableAnimation
+        ? children
+        : children.map((child) => (
+            <FadeIn delay={(delay += delayIncrease)}>{child}</FadeIn>
+          ))}
     </section>
   );
 };
