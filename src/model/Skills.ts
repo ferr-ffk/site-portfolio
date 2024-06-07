72e92fc18ea2c14af1061bd22e553245bbcd4e26
Fernando Freitas <freitax.li@gmail.com>
2024-06-06 22:37:42 -0300

    feat: added skills list

---
+++ src/model/Skills.ts
@@ -0,0 +1,6 @@
+const s =
+  "Modelagem Conceitual, UX, UI, Office, Python, Java, JavaScript, C, Microsoft, PowerPoint, Access, MySQL, Godot, Servidor Windows, Debian, .NET, Excel, Windows, Spline, Figma, Canva, Comunicação escrita, C#, Unity";
+
+const skills = s.split(", ");
+
+export default skills;
