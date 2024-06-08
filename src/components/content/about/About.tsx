6b60665feeeb94fd0dad5d2ed98c5e12192d1c76
Fernando Freitas <freitax.li@gmail.com>
2024-06-08 12:27:13 -0300

    chore: changed paragraph for better description

--- src/components/content/about/About.tsx
+++ src/components/content/about/About.tsx
@@ -21,7 +21,12 @@ const About = () => {
     <Section id="sobre">
       <div className={styles.media_container}>
         {prefersReducedMotion ? (
-          <Image src="/splineFallback/Escorrega@1-2073x1102.png" alt="" fill style={{zIndex: "-1", objectFit: "contain"}}/>
+          <Image
+            src="/splineFallback/Escorrega@1-2073x1102.png"
+            alt=""
+            fill
+            style={{ zIndex: "-1", objectFit: "contain" }}
+          />
         ) : (
           <Spline scene="https://prod.spline.design/afqunuhRJDW5Ogwc/scene.splinecode" />
         )}
@@ -30,16 +35,17 @@ const About = () => {
         <div className={styles.content_container}>
           <SubHeader>Sobre mim</SubHeader>
           <p>
-            Eu me chamo Fernando. Eu tenho 17 anos e atualmente estou estudando o
-            ensino médio integrado ao técnico em informática no Instituto Federal
-            de São Paulo. No meu tempo livre passo ouvindo música e jogando. Eu
-            amo aprender, e estou sempre aprimorando minhas habilidades em coisas
-            que gosto como tecnologia e cozinhar, algo que estou sempre tentando
-            algo novo. Este aprendizado que gosto de compartilhar com os outros,
-            como já fui monitor de salas de estudo de lógica de programação onde
-            eu estudo, ajudando alunos com conhecimentos em lógica de programação.
-            Aguardo um dia no futuro em que posso prestar assistência a quem mais
-            precisa, principalmente na área de TI.
+            Eu me chamo Fernando. Eu tenho 17 anos e atualmente estou estudando
+            o ensino médio integrado ao técnico em informática no Instituto
+            Federal de São Paulo. No meu tempo livre gosto de passar ouvindo
+            música e jogando. Eu amo aprender, e estou sempre aprimorando minhas
+            habilidades em coisas que gosto como tecnologia e cozinhar, tópicos
+            estou sempre tentando algo novo. Gosto de compartilhar com os outros
+            estes meus conhecimnetos, como já fui monitor de salas de estudo de
+            lógica de programação onde eu estudo, ajudando alunos com
+            conhecimentos em lógica de programação. Aguardo um dia no futuro em
+            que posso prestar assistência a quem mais precisa, principalmente na
+            área de TI.
           </p>
         </div>
       </AnimateOnVisible>
