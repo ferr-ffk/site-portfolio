acf1363064b4980487ddee1eb7bce8b07d499f1e
Fernando Freitas <freitax.li@gmail.com>
2024-06-08 17:52:26 -0300

    fix: fixed typo on font name

--- src/components/text/subHeader/SubHeader.tsx
+++ src/components/text/subHeader/SubHeader.tsx
@@ -2,7 +2,7 @@ import styles from "./SubHeader.module.css";
 
 import localFont from "next/font/local";
 
-const Varien = localFont({ src: "../../../../public/font/VARIEN.ttf" });
+const Varien = localFont({ src: "../../../../public/font/varien.ttf" });
 
 interface Props {
   children: React.ReactNode | React.ReactNode[];
